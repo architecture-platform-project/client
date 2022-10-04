249e338cd801cbd9c1e8a97189184367273b5682
soonzero <sykeum0912@naver.com>
2022-10-04 12:42:37 +0900

    :package: Add: 약관 동의 페이지 추가
    필수 약관 선택 시에만 다음 단계로 진행 가능

--- src/routes/Auth/index.tsx
+++ src/routes/Auth/index.tsx
@@ -1,8 +1,13 @@
 import Login from './Login';
+import Terms from './Terms';
 
 export const authRoutes = [
 	{
 		path: '/login',
 		element: Login,
+	},
+	{
+		path: '/signup/terms',
+		element: Terms,
 	},
 ];
